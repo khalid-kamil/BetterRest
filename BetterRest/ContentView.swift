c88936ef3e896df57e1730ef5fbd73a527bbb723
Khalid Kamil <carts_carps_0o@icloud.com>
2022-01-19 23:53:49 +0000

    Replaced VStacks in form with Sections. Set calculation function to run on change of wake up time, coffee amount and desired sleep amount.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,13 +5,90 @@
 //  Created by Khalid Kamil on 25/12/2021.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    @State private var wakeUp = defaultWakeTime
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
     }
+    
+    var body: some View {
+        NavigationView {
+                Form {
+                    Section(header: Text("When do you want to wake up?")) {
+                        DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                            .labelsHidden()
+                            .onChange(of: wakeUp) { _ in
+                                calculateBedtime()
+                            }
+                    }
+                    Section(header: Text("Desired amount of sleep")) {
+                        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                            .onChange(of: sleepAmount) { _ in
+                                calculateBedtime()
+                            }
+                    }
+                    Section(header: Text("Daily coffee intake")) {
+                        Picker(selection: $coffeeAmount, label: Text("Number of Cups")) {
+                            ForEach(1...20, id: \.self) { cup in
+                                Text(cup == 1 ? "1 cup" : "\(cup) cups")
+                            }
+                        }
+                        .onChange(of: coffeeAmount) { _ in
+                            calculateBedtime()
+                        }
+                    }
+                    Section(header: Text("Recommended Bedtime")) {
+//                        Text(alertTitle)
+                        Text(alertMessage)
+                    }
+                }
+                .navigationTitle("BetterRest")
+                .toolbar {
+                    Button("Calculate", action: calculateBedtime)
+                }
+//                .alert(alertTitle, isPresented: $showingAlert) {
+//                    Button("OK") {}
+//                } message: {
+//                    Text(alertMessage)
+//                }
+        }
+    }
+    
+    func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        
+        showingAlert = true
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
